55911b25ec05e8485fa8a044c88f1ac518505c0a
imrancs10 <imran.cs10@gmail.com>
2019-09-03 00:12:55 +0530

    lazy loading disabled

--- DataLayer/DbEntity.Context.cs
+++ DataLayer/DbEntity.Context.cs
@@ -18,6 +18,7 @@ namespace DataLayer
         public PatientPortalApiEntities()
             : base("name=PatientPortalApiEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
